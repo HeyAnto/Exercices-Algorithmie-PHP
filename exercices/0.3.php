e0ea3e6c404b8a24615656b21d736c033916e3fa
HeyAnto <antonin63170@gmail.com>
2025-02-03 20:21:27 +0100

    Exercice 0.3

--- exercices/0.3.php
+++ exercices/0.3.php
@@ -1,13 +1,13 @@
 <?php include_once "../includes/head.php" ?>
 
 <head>
-    <title>Exercices 0.2</title>
+    <title>Exercices 0.3</title>
 </head>
 
 <body>
     <main>
         <section>
-            <h1>Exercice 0.2</h1>
+            <h1>Exercice 0.3</h1>
         </section>
 
         <section class="mt-50">
@@ -17,9 +17,14 @@
                 et
                 <mark>else if</mark>
                 pour créer deux conditions qui vérifient la valeur de la variable
-                <mark>$genre</mark>.
-                En fonction des résultats de ces comparaisons,
-                j'affiche le message correspondant indiquant si la personne est un homme ou une femme.
+                <mark>$gender</mark>.
+                <br>
+                <br>
+                J'ai utilisé les variables
+                <mark>$age</mark>
+                et
+                <mark>$height</mark>
+                pour insérer dynamiquement ces valeurs dans les messages.
             </p>
         </section>
 
@@ -27,11 +32,13 @@
             <h2>Résultat</h2>
             <?php
             $gender = "male";
+            $height = 175;
+            $age = 21;
 
             if ($gender === "male") {
-                echo "Je suis un homme.";
+                echo "J'ai $age ans, je mesure $height centimètres et je suis un homme.";
             } else if ($gender === "female") {
-                echo "Je suis une femme.";
+                echo "J'ai $age ans, je mesure $height centimètres et je suis une femme.";
             }
             ?>
         </section>
--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <section class="flex flex-row flex-wrap gap-10 mt-50">
             <a class="btn btn-primary" href="/exercices/0.1.php">Exercice 0.1</a>
             <a class="btn btn-primary" href="/exercices/0.2.php">Exercice 0.2</a>
+            <a class="btn btn-primary" href="/exercices/0.3.php">Exercice 0.3</a>
         </section>
 
         <section class="flex flex-row gap-20 mt-50">
