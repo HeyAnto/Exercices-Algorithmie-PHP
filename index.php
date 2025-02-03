1de1985a7cae535d0aeb63a6b7f3f06c58109f8c
HeyAnto <antonin63170@gmail.com>
2025-02-03 15:31:18 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include_once "./includes/head.php" ?>
+
+<body>
+    <main>
+        <section>
+            <h1>Exercices Algorithmie PHP</h1>
+            <h2>Index</h2>
+        </section>
+        <section class="flex flex-row flex-wrap gap-10 mt-50">
+            <a class="btn btn-primary" href="#">#</a>
+        </section>
+        <section class="flex flex-row gap-20 mt-50">
+            <a class="btn-link" href="https://gitlab.com/JulesThePooh/exercices-algorithmie-js" target="_blank">
+                Consignes
+                <img src="/assets/icon/icon-link.svg" alt="Icone">
+            </a>
+            <a class="btn-link" href="https://github.com/HeyAnto/Exercices-Algorithmie-PHP" target="_blank">
+                GitHub
+                <img src="/assets/icon/icon-link.svg" alt="Icone">
+            </a>
+        </section>
+    </main>
+</body>
+
+</html>
